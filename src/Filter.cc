5ec0b499c05226dbd9b7f3e37d76486b86f0f18b
Oleg Sazonov <osaz1423@gmail.com>
2020-12-18 23:01:24 +0300

    Initial push

---
+++ src/Filter.cc
@@ -0,0 +1,46 @@
+/* Copyright (C) 2020 Oleg Sazonov */
+#include <iostream>
+#include <cmath>
+#include "Filter.hh"
+
+Filter::Filter(int GainFreq, int gain, enum FilterType filt, unsigned smpl, double Q) {
+    const double A = sqrt(pow(10.0, (gain)/20.0));
+    const double w = 2 * M_PI* GainFreq/smpl;
+    const double alpha = sin(w)/(2 * Q);
+    switch (filt) {
+            case LOW_SHELF:
+                b0 = A * ((A+1) - (A-1)*cos(w) + 2*sqrt(A)*alpha);
+                b1 = 2*A * ((A-1) - (A+1)*cos(w));
+                b2 = A * ((A+1) - (A-1)*cos(w) - 2*sqrt(A)*alpha);
+                a0 = (A+1) + ((A-1)*cos(w)) + 2*sqrt(A)*alpha;
+                a1 = -2 * ((A-1) + (A+1)*cos(w));
+                a2 = (A+1) + (A-1)*cos(w) - 2*sqrt(A)*alpha;
+                break;
+            case HIGH_SHELF:
+                b0 = A * ((A+1) + (A-1)*cos(w) + 2*sqrt(A)*alpha);
+                b1 = -2*A * ((A-1) + (A+1)*cos(w));
+                b2 = A * ((A+1) + (A-1)*cos(w) - 2*sqrt(A)*alpha);
+                a0 = (A+1) - ((A-1)*cos(w)) + 2*sqrt(A)*alpha;
+                a1 = 2 * ((A-1) - (A+1)*cos(w));
+                a2 = (A+1) - (A-1)*cos(w) - 2*sqrt(A)*alpha;
+                break;
+            default:
+            cerr << "[FILT] No way! U just don't selected filter type!" << endl;
+            exit(0);      
+    }
+    this->filt = filt;
+    this->sample_freq = smpl;
+}
+
+vector<double>* Filter::process(vector<double> *in) {
+    static vector<double> output(in->size());
+    double bin1 = 0, bin2 = 0, bout1 = 0, bout2 = 0;
+    for(int i = 0; i < output.size(); i++) {
+        output.at(i) = (b0 / a0) * in->at(i) + (b1 / a0) * bin1 + (b2 / a0) * bin2 - (a1 / a0) * bout1 - (a2 / a0) * bout2;
+        bin2 = bin1;
+        bin1 = in->at(i);
+        bout2 = bout1;
+        bout1 = output[i];
+    }
+    return &output;
+}
---
+++ src/Filter.hh
@@ -0,0 +1,26 @@
+/* Copyright (C) Oleg Sazonov */
+
+#pragma once
+#include <cstdint>
+#include <vector>
+using namespace std;
+
+enum FilterType {
+    LOW_SHELF,
+    HIGH_SHELF
+};
+
+class Filter {
+
+    private:
+    unsigned sample_freq;
+    float a0,a1,a2,b0,b1,b2;
+    double delay1[2] = {0.0f}, delay2[2] = {0.0f};
+    enum FilterType filt;
+
+    public:
+    Filter(int GainFreq, int gain, enum FilterType filt, unsigned smpl, double Q);
+    vector<double> samples;
+    vector<double>* process(vector<double>* in);
+
+};
